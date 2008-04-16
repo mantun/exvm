75b7100f455d115eab858d21dfa6cac9f6585d12
progexp <progexp@603557b2-6635-0410-bec2-e7f8933db683>
2008-04-16 17:15:35 +0000

    renamed to exvm, added tests

--- trunk/Registers.pas
+++ trunk/Registers.pas
@@ -4,32 +4,18 @@ interface
 
 uses Common;
 
-function getreg(index : Byte; size : TOpSize) : TCell;
-procedure setreg(index : Byte; size : TOpSize; value : TCell);
+type
+  TRegistersFile = Array[Byte] of TCell;
+var
+  RegistersFile : TRegistersFile;
+
+procedure Clear;
 
 implementation
 
-var
-  RegisterFile : Array[Byte] of Cardinal;
-
-function getreg(index : Byte; size : TOpSize) : TCell;
+procedure Clear;
 begin
-  case size of
-    sz32 : Result := P32Cell(@(RegisterFile[index]))^;
-    sz16 : Result := P16Cell(@(RegisterFile[index]))^;
-    sz8 : Result := P8Cell(@(RegisterFile[index]))^;
-    else begin Assert(False); Result := 0; end;
-  end
-end;
-
-procedure setreg(index : Byte; size : TOpSize; value : TCell);
-begin
-  case size of
-    sz32 : P32Cell(@(RegisterFile[index]))^:= value;
-    sz16 : P16Cell(@(RegisterFile[index]))^:= value;
-    sz8 : P8Cell(@(RegisterFile[index]))^:= value;
-    else Assert(False);
-  end
+  FillChar(RegistersFile, SizeOf(RegistersFile), 0);
 end;
 
 end.
